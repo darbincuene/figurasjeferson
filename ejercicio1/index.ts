2b40ecc28b86109a3c3bca71f3ab97ff7ef80d83
quimbaya17 <john.quimbaya1@misena.edu.co>
2024-02-14 18:36:39 -0500

    actualizacion

---
+++ ejercicio1/index.ts
@@ -0,0 +1,10 @@
+import { Rectangulo } from './index1'
+
+
+const miRectangulo = new Rectangulo(5, 10);
+
+
+console.log("Área del rectángulo:", miRectangulo.calcularArea());
+
+
+console.log("Perímetro del rectángulo:", miRectangulo.calcularPerimetro());
---
+++ ejercicio1/index1.ts
@@ -0,0 +1,44 @@
+ export class Rectangulo {
+
+    base: number
+    altura: number
+
+
+
+
+    constructor(base: number, altura: number) {
+        this.base = base
+        this.altura = altura
+        
+
+
+    }
+
+    getBase() {
+        return this.base
+
+    }
+    setBase(base: number) {
+        this.base = base
+
+    }
+    getAltura() {
+        return this.altura
+    }
+    setAltura(altura: number) {
+        this.altura = altura
+
+    }
+
+    calcularArea(): number {
+        return this.base * this.altura
+     }
+     
+calcularPerimetro(): number {
+        
+       
+     
+return 2 * (this.base + this.altura);
+    }
+
+}
